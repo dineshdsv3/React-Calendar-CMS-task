d0525258c4d20caadf4bb2073c9b23fbda466104
dineshdsv3 <dineshdsv3@gmail.com>
2020-06-06 02:11:57 +0530

    Created a calendar using react-big-calendar

---
+++ src/MyCalendar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import moment from 'moment'
+
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+const localizer = momentLocalizer(moment);
+
+class MyCalendar extends React.Component {
+
+	state = {
+		events: []
+	}
+	
+	render() {
+		return (
+			<div>
+				<Calendar
+					localizer={localizer}
+					startAccessor="start"
+					endAccessor="end"
+					style={{ height: '420px' }}
+					events={[]}
+				/>
+			</div>
+		);
+	}
+}
+
+export default MyCalendar;
