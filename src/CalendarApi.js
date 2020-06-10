f770e9b54a1e820ee130f92b8b86b8eb2ea25c01
dineshdsv3 <dineshdsv3@gmail.com>
2020-06-10 16:13:47 +0530

    calendar events and manual events added

--- src/CalendarApi.js
+++ src/CalendarApi.js
@@ -3,28 +3,54 @@ import ApiCalendar from 'react-google-calendar-api';
 import MyCal1 from './MyCal1';
 
 class CalendarApi extends React.Component {
-	state = {
-		events: [],
+	constructor(props) {
+		super(props);
+		this.state = {
+			events: [],
+			sign :ApiCalendar.sign
+		};
+		ApiCalendar.onLoad(() => {
+			this.signUpdate(ApiCalendar.sign);
+			// console.log(`Api calendar is ${ApiCalendar.sign}`)
+			ApiCalendar.listenSign(this.signUpdate.bind(this));
+		});
+	}
+
+	signUpdate = (sign) => {
+		// console.log(`Sign update is ${sign}`)
+		this.setState({
+			sign
+		})
+		this.listenEvents();
+	}
+
+	componentDidMount() {
+		this.listenEvents()
+	}
+
+
+	listenEvents = async () => {
+		if (ApiCalendar.sign) {
+			const resp = await ApiCalendar.listUpcomingEvents(10);
+			const events = resp.result.items.map((ele) => {
+				return { start: ele.start.dateTime, end: ele.end.dateTime, title: ele.summary };
+			});
+			this.setState({
+				events
+			});
+			
+		}
 	};
 
-
-	handleClickItem = async (e, name) => {
+	handleClickItem = (e, name) => {
 		if (name === 'sign-in') {
-			await ApiCalendar.handleAuthClick();
-			 ApiCalendar.listUpcomingEvents(10).then((result) => {
-				if (result.status === 200) {
-					// console.log(result.result.items)
-					result.result.items.map((ele) => {
-						let event = { start: ele.start.dateTime, end: ele.end.dateTime, title: ele.summary };
-						this.setState({ events: [...this.state.events, event] });
-					});
-				}
-			});
+			ApiCalendar.handleAuthClick();
+			ApiCalendar.listenSign(this.listenEvents);
 		}
 	};
 
 	render() {
-		return this.state.events.length > 0 ? (
+		return this.state.sign ? (
 			<div>
 				<MyCal1 events={this.state.events} />
 			</div>
--- src/MyCal1.js
+++ src/MyCal1.js
@@ -6,26 +6,50 @@ import interactionPlugin from '@fullcalendar/interaction';
 
 class MyCal1 extends React.Component {
 	state = {
-		events: [],
+		events: [{ start: '2020-06-12T10:00:00+05:30', end: '2020-06-12T15:00:00+05:30', title: 'Sample 1' }],
 	};
-	componentDidMount() {
-		this.setState({events:this.props.events})
+
+	componentWillReceiveProps(nextProps) {
+		nextProps.events.map((ele) => {
+			this.setState({
+				events: [...this.state.events, ele],
+			});
+		});
 	}
-	
+
+	handleChange = (e) => {
+		e.preventDefault();
+		console.log(e.target.value);
+	};
+
 	render() {
 		return (
-            <div className='demo-app-calendar'>
-			<FullCalendar
-				defaultView="timeGridWeek"
-				header={{
-					left: 'prev,next today',
-					center: 'title',
-					right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
-				}}
-				events={this.state.events}
-				plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-			/>
-            </div>
+			<div>
+				<div className="ui container center aligned">
+					<div className="ui input">
+						<label htmlFor="startTime">Start Time</label>
+						<input type="time" id="startTime" name="startTime" onChange={this.handleChange} />
+					</div>
+					<div className="ui input">
+						<label htmlFor="endTime">End Time</label>
+						<input type="time" id="endTime" name="endTime" onChange={this.handleChange} />
+					</div>
+				</div>
+
+				<div className="demo-app-calendar">
+					{console.log(this.state.events)}
+					<FullCalendar
+						defaultView="timeGridWeek"
+						header={{
+							left: 'prev,next today',
+							center: 'title',
+							right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
+						}}
+						events={this.state.events}
+						plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+					/>
+				</div>
+			</div>
 		);
 	}
 }
